758b33ad84a0adbd3c001f0ecc702272006d27c0
mizterwaffles <nhillhouse04@gmail.com>
2025-10-24 09:33:18 -0600

    Added comment and described to the user basic program functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Nathan Hillhouse
+10/24/2025
+Lab 8 - Maze
+*/
+
+
+Console.Clear();
+Console.WriteLine("Welcome to the Maze! You will use your arrow keys to navigate the maze.");
